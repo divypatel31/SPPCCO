c12b29bc5d11c48f0b5fce5ca003afff0bbd4d88
Divy <divypateln@gmail.com>
2026-03-25 00:04:31 +0530

    Updated backend URL to Render

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 /* Middleware */
 app.use(cors({
-    origin: ["http://localhost:5173", "https://sppcco.vercel.app/"],
+    origin: ["http://localhost:5173", "https://sppcco.vercel.app"],
     credentials: true
 }));
 
