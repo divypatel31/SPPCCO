872b9705c26488b7c3e179b49e081a96377afa95
Divy <divypateln@gmail.com>
2026-03-26 11:38:06 +0530

    Email otp error solve

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,15 +1,14 @@
-// server/utils/sendEmail.js
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 587,      
-      secure: false,  
+      port: 465,      
+      secure: true,  
       auth: {
         user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS 
+        pass: process.env.EMAIL_PASS  
       },
       tls: {
         rejectUnauthorized: false 
